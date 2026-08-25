9f60eb21e9ddefc5b3f0c712a9fb443dcb44c104
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-25 15:16:26 +0100

    style: format feature package exports

--- src/bp_engine/features/__init__.py
+++ src/bp_engine/features/__init__.py
@@ -1,5 +1,9 @@
 from bp_engine.features.models import FEATURE_VERSION, FeatureTarget, MarketFeature
-from bp_engine.features.repository import FeatureConflict, FeatureStoreResult, MarketFeatureRepository
+from bp_engine.features.repository import (
+    FeatureConflict,
+    FeatureStoreResult,
+    MarketFeatureRepository,
+)
 
 __all__ = [
     "FEATURE_VERSION",
