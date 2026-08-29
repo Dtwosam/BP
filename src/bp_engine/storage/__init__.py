c322c0832e68dc580b1f5b62a3d217a829638a34
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-29 10:51:27 +0100

    feat: expose phase13 storage tables

--- src/bp_engine/storage/__init__.py
+++ src/bp_engine/storage/__init__.py
@@ -1,6 +1,11 @@
 """Storage primitives for BP."""
 
 from bp_engine.storage import schema as schema
+from bp_engine.storage.improvement_schema import (
+    improvement_evaluations,
+    improvement_experiments,
+    improvement_promotion_decisions,
+)
 from bp_engine.storage.paper_schema import (
     paper_fills,
     paper_order_terminal_events,
@@ -12,3 +17,6 @@ schema.paper_orders = paper_orders
 schema.paper_fills = paper_fills
 schema.paper_order_terminal_events = paper_order_terminal_events
 schema.paper_settlements = paper_settlements
+schema.improvement_experiments = improvement_experiments
+schema.improvement_evaluations = improvement_evaluations
+schema.improvement_promotion_decisions = improvement_promotion_decisions
