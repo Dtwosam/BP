9d563af61e5409a108f305198d47ece68c15af47
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-29 10:38:02 +0100

    feat: expose phase13 improvement contract

---
+++ src/bp_engine/improvement/__init__.py
@@ -0,0 +1,21 @@
+from bp_engine.improvement.models import (
+    DECISION_VERSION,
+    EVALUATION_VERSION,
+    EXPERIMENT_VERSION,
+    ChampionRef,
+    ChangeFamily,
+    EvidenceRole,
+    ImprovementExperimentSpec,
+    PromotionDecision,
+)
+
+__all__ = [
+    "DECISION_VERSION",
+    "EVALUATION_VERSION",
+    "EXPERIMENT_VERSION",
+    "ChampionRef",
+    "ChangeFamily",
+    "EvidenceRole",
+    "ImprovementExperimentSpec",
+    "PromotionDecision",
+]
